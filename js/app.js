c1aef6f22b54a6c1abb26a9a1185d5fec5a6d4b7
Dima <dima_kurinnoy@mail.ru>
2021-08-22 19:46:08 +0200

    almost complict

---
+++ js/app.js
@@ -0,0 +1,57 @@
+$(function(){
+        /*filter*/
+      let filter = $("[data-filter]");
+
+      filter.on("click",function(event){
+          event.preventDefault();
+          let cat = $(this).data('filter');
+          if(cat == 'all'){
+              $("[data-cat]").removeClass("hide");
+          }else{
+
+              $("[data-cat]").each(function(){
+              let workCat = $(this).data('cat');
+              if(workCat != cat){
+                  $(this).addClass('hide');
+                } else{
+                    $(this).removeClass('hide');
+                }
+
+          })
+        }
+
+        
+      })
+
+      /*modal*/
+      let modalClose =$("[data-close]")
+      let modalCall =$("[data-modal]")
+      modalCall.on("click",function(event){
+          event.preventDefault();
+          let $this =$(this);
+          let modalId =$this.data('modal');
+          $(modalId).addClass('show')
+          $("body").addClass('no-scroll')
+          
+      })
+      modalClose.on("click",function(event){
+          event.preventDefault();
+          let $this= $(this)
+          let modalParent = $this.parents('.modal');
+
+          modalParent.removeClass('show')
+          $("body").removeClass('no-scroll')
+
+      })
+
+      $(".modal").on("click",function(){
+          $(this).removeClass('show')
+          $("body").removeClass('no-scroll')
+      })
+      $(".modal__dialog").on("click",function(event){
+          event.stopPropagation();
+      })
+
+
+
+});
